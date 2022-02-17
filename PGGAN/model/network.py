71851fbbd62fc79edfa412aa721c9cbd971a0f61
ssail09 <ssail@naver.com>
2022-02-17 15:05:30 +0900

    network

---
+++ PGGAN/model/network.py
@@ -0,0 +1,30 @@
+
+import torch
+import torch.nn as nn
+class generator(nn.module):
+    def __init__(self,
+                dimLatent,
+                depthscale0):
+        super(generator, self).__init__()
+        self.dimLatent = dimLatent
+        self.depthscale0 = depthscale0
+        # custom_layer linear, conv2d, normalization layer
+        # scale 0 layer
+    def add_sacle():
+        pass
+    def foward():
+        pass
+
+
+
+
+class discriminator(nn.module):
+    def __init__(self,):
+        pass
+    def initLayer():
+        pass
+    def add_sacle():
+        pass
+    def foward():
+        pass
+
