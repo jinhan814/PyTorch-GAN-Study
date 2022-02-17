201f1d3dcf1efcfa805a2c211ab3ae502ea38dfc
ssail09 <ssail@naver.com>
2022-02-17 14:46:12 +0900

    network.py

--- PGGAN/model/network.py
+++ PGGAN/model/network.py
@@ -1,1 +1,31 @@
 
+import torch
+import torch.nn as nn
+class generator(nn.module):
+    def __init__(self,
+                dimLatent,
+                depthscale0):
+        super(generator, self).__init__()
+        self.dimLatent = dimLatent
+        self.depthscale0 = depthscale0
+    
+    def initLayer():
+        pass
+    def add_sacle():
+        pass
+    def foward():
+        pass
+
+
+
+
+class discriminator(nn.module):
+    def __init__(self,):
+        pass
+    def initLayer():
+        pass
+    def add_sacle():
+        pass
+    def foward():
+        pass
+
