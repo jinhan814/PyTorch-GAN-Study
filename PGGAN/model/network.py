fe5bdfece1b483b0d48674852630b7b3dc2df8bf
eric9687 <eric9687@naver.com>
2022-02-17 14:46:58 +0900

    .

--- PGGAN/model/network.py
+++ PGGAN/model/network.py
@@ -1,1 +1,13 @@
+import torch
+import torch.nn as nn
 
+class GNet(nn.Module):
+    def __init__(self,
+                 dimLatent,
+                 depthScale0,
+                 initBiasToZero=True,
+                 leakyReluLeak=0.2,
+                 normalization=True,
+                 generationActivation=None,
+                 dimOutput=3,
+                 equalizedlR=True):
