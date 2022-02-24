ee098afcfe3269fa1bae6faa326aa3ba375d5e52
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2022-02-24 16:01:59 +0900

    add GNet

--- PGGAN/model/network.py
+++ PGGAN/model/network.py
@@ -1,21 +1,63 @@
-
 import torch
 import torch.nn as nn
-class generator(nn.module):
+import torch.nn.functional as F
+from .custom_layer import NormalizationLayer, EqualizedLinear, EqualizedConv2d, Upsampling
+
+class GNet(nn.Module):
     def __init__(self,
-                dimLatent,
-                depthscale0):
-        super(generator, self).__init__()
+                 dimLatent=512,
+                 leakyReluLeak=0.2,
+                 dimOutput=3):
+        super(GNet, self).__init__()
+        '''
+        things to do
+
+        1. latent vector(512 * 1 * 1) -> (512 * 4 * 4) (fc + view)
+        2. scale 별 upsample + conv + @(normalization, lReLU, ...)
+        3. toRGB 적용 + alpha 비율대로 합치기
+        '''
         self.dimLatent = dimLatent
-        self.depthscale0 = depthscale0
-        # custom_layer linear, conv2d, normalization layer
-        # scale 0 layer
-    def add_sacle():
-        pass
-    def foward():
-        pass
+        self.leakyRelu = nn.LeakyReLU(leakyReluLeak)
+        self.dimOutput = dimOutput
+        self.alpha = 0
+        self.normalizationLayer = NormalizationLayer()
+        self.formatLayer = EqualizedLinear(self.dimLatent,
+                                           16 * self.dimLatent)
+        
+        # scale별 layer block
+        self.scaleLayers = nn.ModuleList()
+        self.scaleLayers.append(nn.ModuleList([
+            EqualizedConv2d(dimLatent,
+                            dimLatent,
+                            3,
+                            padding=1),
+            self.leakyRelu,
+            self.normalizationLayer
+        ]))
 
+        # scale별 toRGB layer
+        self.toRGBLayer = nn.ModuleList()
+    
+    def addScale(self, depthNewScale):
+        pass
+    
+    def forward(self, x):
+        x = self.normalizationLayer(x)
+        x = x.view(-1, torch.prod(x.size()[1:]))
+        x = self.leakyRelu(self.formatLayer(x))
+        x = x.view(x.size()[0], -1, 4, 4)
+        x = self.normalizationLayer(x)
 
+        for scale, scaleLayer in enumerate(self.scaleLayers):
+            x = scaleLayer(x)
+            if 1: # 마지막에서 두 번째 레이어이면서 alpha값으로 합쳐야하는 경우. (나중에 작성하기)
+                y = Upsampling(x)
+                y = self.toRGBLayers[-1](x)
+        
+        x = self.toRGBLayers[-1](x)
+        if self.alpha > 0:
+            x = self.alpha * y + (1 - self.alpha) * x
+        return x
 
 
 class discriminator(nn.module):
