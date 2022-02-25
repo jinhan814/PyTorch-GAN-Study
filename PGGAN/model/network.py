81589f755706258b981da90a6f51115fc9522523
eric9687 <eric9687@naver.com>
2022-02-25 23:39:58 +0900

    add  DNet

--- PGGAN/model/network.py
+++ PGGAN/model/network.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from .custom_layer import NormalizationLayer, EqualizedLinear, EqualizedConv2d, Upsampling
+from .custom_layer import NormalizationLayer, EqualizedLinear, EqualizedConv2d, Upsampling, Downsampling
 
 class GNet(nn.Module):
     def __init__(self,
@@ -27,8 +27,8 @@ class GNet(nn.Module):
         # scale별 layer block
         self.scaleLayers = nn.ModuleList()
         self.scaleLayers.append(nn.ModuleList([
-            EqualizedConv2d(dimLatent,
-                            dimLatent,
+            EqualizedConv2d(self.dimLatent,
+                            self.dimLatent,
                             3,
                             padding=1),
             self.leakyRelu,
@@ -37,9 +37,33 @@ class GNet(nn.Module):
 
         # scale별 toRGB layer
         self.toRGBLayer = nn.ModuleList()
+        
+        # scale depth 리스트
+        self.scaledepths = [dimLatent]
     
     def addScale(self, depthNewScale):
-        pass
+        
+        depthLastScale = self.scalesDepth[-1]
+
+        self.scalesDepth.append(depthNewScale)
+
+        self.scaleLayers.append(nn.ModuleList())
+
+        self.scaleLayers[-1].append(EqualizedConv2d(depthLastScale,
+                                                    depthNewScale,
+                                                    3,
+                                                    padding=1))
+        self.scaleLayers[-1].append(self.leakyRelu)
+        self.scaleLayers[-1].append(self.normalizationLayer)
+        
+        self.scaleLayers[-1].append(EqualizedConv2d(depthNewScale, depthNewScale,
+                                                    3, padding=1))
+        self.scaleLayers[-1].append(self.leakyRelu)
+        self.scaleLayers[-1].append(self.normalizationLayer)
+
+        self.toRGBLayer.append(EqualizedConv2d(depthNewScale,
+                                                self.dimOutput,
+                                                1))
     
     def forward(self, x):
         x = self.normalizationLayer(x)
@@ -50,23 +74,54 @@ class GNet(nn.Module):
 
         for scale, scaleLayer in enumerate(self.scaleLayers):
             x = scaleLayer(x)
-            if scale == len(self.scaleLayers) - 2 and alpha > 0:
+            if scale == len(self.scaleLayers) - 2 and self.alpha > 0:
                 y = Upsampling(x)
                 y = self.toRGBLayers[-1](x)
         
-        x = self.toRGBLayers[-1](x)
+        x = self.toRGBLayer[-1](x)
         if self.alpha > 0:
             x = self.alpha * y + (1 - self.alpha) * x
         return x
 
 
-class discriminator(nn.module):
-    def __init__(self,):
-        pass
-    def initLayer():
-        pass
+class DNet(nn.module):
+    def __init__(self,depthScale0,
+                 leakyReluLeak=0.2,
+                 dimInput=3):
+        self.depthscale0 = depthScale0
+        self.dimInput = dimInput
+        self.leakyRelu = nn.LeakyReLU(leakyReluLeak)
+        self.alpha = 0
+        self.decisionlayer = EqualizedLinear(self.depthScale0,1)   # depthscale0 = 512
+        
+        self.groupscalezero = EqualizedLinear(depthScale0 *4 *4,
+                                              depthScale0)
+        
+        self.scaleLayers = nn.ModuleList()
+        self.scaleLayers.append(nn.ModuleList([
+            EqualizedConv2d(self.depthScale0,
+                            self.depthScale0,
+                            3,
+                            padding=1),
+            self.leakyRelu
+        ]))
+        
+        self.fromRGBlayer = nn.ModuleList()
+        self.fromRGBlayer.append(EqualizedConv2d(self.dimInput,
+                                                 self.depthScale0))
+        
+        '''
+        우리는 오늘 장렬히 일주일을 마무리하겠다.
+        '''
+        
     def add_sacle():
         pass
-    def foward():
+    def foward(self, x):
+        
+        if self.alpha > 0    ##### 모르겠당
+            y = Downsampling(x)
+            y = self.fromRGBlayer[](y)
+            
+        
         pass
 
